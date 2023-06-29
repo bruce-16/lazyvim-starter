9e2559281a37805979ebfdba72aca8ffdd1f0a2f
Zhe Zhang <zz__0123@163.com>
2023-06-29 15:21:26 +0800

    fix: tmux navigator config

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,6 +1,16 @@
 return {
   -- tmux navigator
   {
-    "aserowy/tmux.nvim",
+    "alexghergh/nvim-tmux-navigation",
+    keys = function()
+      local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+      return {
+        { "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft, "left" },
+        { "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown, "Down" },
+        { "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp, "Up" },
+        { "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight, "right" },
+      }
+    end,
   },
 }
